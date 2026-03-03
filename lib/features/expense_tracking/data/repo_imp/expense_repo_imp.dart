8fe703fcf768e101db754ac4da981d5dbf06ba18
manojsrinivasan002 <manojsrinivasan666@gmail.com>
2026-03-03 16:54:33 +0530

    fix: update expense repository implementation

--- lib/features/expense_tracking/data/repo_imp/expense_repo_imp.dart
+++ lib/features/expense_tracking/data/repo_imp/expense_repo_imp.dart
@@ -1,24 +1,44 @@
+import 'package:dartz/dartz.dart';
+import 'package:spend_wise/core/error/exceptions.dart';
+import 'package:spend_wise/core/error/failure.dart';
+import 'package:spend_wise/features/expense_tracking/data/datasouces/expense_local_datasource.dart';
+import 'package:spend_wise/features/expense_tracking/data/models/expense_model.dart';
 import 'package:spend_wise/features/expense_tracking/domain/entities/expense_entity.dart';
 import 'package:spend_wise/features/expense_tracking/domain/repo/expense_repo.dart';
 
-class ExpenseRepoImp extends ExpenseRepo{
+class ExpenseRepoImp extends ExpenseRepo {
+  final ExpenseLocalDatasource localDatasource;
+
+  ExpenseRepoImp({required this.localDatasource});
   @override
-  Future<void> deleteExpense(String id) {
-    // TODO: implement deleteExpense
-    throw UnimplementedError();
+  Future<Either<Failure, void>> deleteExpense(String id) async {
+    try {
+      await localDatasource.deleteCachedExpense(id);
+      return Right(null);
+    } on CacheException catch (e) {
+      return Left(CacheFailure(e.message));
+    }
   }
 
   @override
-  Future<List<ExpenseEntity>> getExpenses() {
-    // TODO: implement getExpenses
-    throw UnimplementedError();
+  Future<Either<Failure, List<ExpenseEntity>>> getExpenses() async {
+    try {
+      final expenseModels = await localDatasource.getCachedExpenses();
+      final entities = expenseModels.map((model) => model.toEntity()).toList();
+      return right(entities);
+    } on CacheException catch (e) {
+      return Left(CacheFailure(e.message));
+    }
   }
 
   @override
-  Future<void> saveExpense(ExpenseEntity expense) {
-    // TODO: implement saveExpense
-    throw UnimplementedError();
+  Future<Either<Failure, void>> saveExpense(ExpenseEntity expense) async {
+    try {
+      final expenseModel = ExpenseModel.fromEntity(expense);
+      await localDatasource.cacheExpense(expenseModel);
+      return right(null);
+    } on CacheException catch (e) {
+      return Left(CacheFailure(e.message));
+    }
   }
-  
-
 }
